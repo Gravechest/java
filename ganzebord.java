8f3732d3a890d88d5046a6c2a83705cb21806422
Gravechest <75589056+Gravechest@users.noreply.github.com>
2022-10-12 13:48:47 +0200

    Create ganzebord.java

---
+++ ganzebord.java
@@ -0,0 +1,244 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Player{
+    static int Count;
+    static int Beurt;
+
+    int playersBehind;
+    boolean beurtOverslaan;
+    int pos;
+    String kleur;
+    void movePlayer(int pos){
+        for(int i = 0;i < Player.Count;i++){
+            if(i != Player.Beurt && Main.player[i].pos == pos){
+                return;
+            }
+        }
+        this.pos = pos;
+    }
+}
+
+class Vak{
+    static final byte NORMAL = 0;
+    static final byte HERHAAL = 1;
+    static final byte BRUG = 2;
+    static final byte HERBERG = 3;
+    static final byte PUT = 4;
+    static final byte VERDWAALD = 5;
+    static final byte GEVANGENIS = 6;
+    static final byte DOOD = 7;
+    static final byte FINISH = 8;
+    static final byte TE_VER = 9;
+    static final byte PLAYER = (byte)0x80;
+}
+
+public class Main {
+    static final int BOARDSZ = 64;
+    static byte[] board = new byte[BOARDSZ+12];
+    static Player[] player;
+    static int rndSeed;
+    static int throwDice(){
+        int t = (int)(System.currentTimeMillis()+rndSeed);
+        t ^= t >> 3;
+        t ^= t << 7;
+        t ^= t >> 2;
+        rndSeed++;
+        return t % 6 + 1;
+    }
+    static void printBoard(){
+        for(int i = 0;i < Player.Count;i++){
+            board[player[i].pos] |= Vak.PLAYER;
+        }
+        for(int i = 0;i < BOARDSZ;i+=9) {
+            for (int i2 = 0;i2 < 9;i2++) {
+                System.out.print('[');
+                if ((board[i+i2] & (byte) 0x80) == 0) {
+                    switch (board[i+i2]) {
+                        case Vak.HERHAAL:
+                            System.out.print('G');
+                            break;
+                        case Vak.BRUG:
+                            System.out.print('B');
+                            break;
+                        case Vak.HERBERG:
+                            System.out.print('H');
+                            break;
+                        case Vak.PUT:
+                            System.out.print('P');
+                            break;
+                        case Vak.VERDWAALD:
+                            System.out.print('D');
+                            break;
+                        case Vak.GEVANGENIS:
+                            System.out.print('#');
+                            break;
+                        case Vak.DOOD:
+                            System.out.print('!');
+                            break;
+                        default:
+                            System.out.print(' ');
+                            break;
+                    }
+                }
+                else {
+                    System.out.print('@');
+                }
+                System.out.print(']');
+            }
+            System.out.print("\n");
+        }
+        for(int i = 0;i < Player.Count;i++){
+            board[player[i].pos] &= ~Vak.PLAYER;
+        }
+    }
+    static void playerTurn(int id,int posBuf){
+        switch(board[posBuf]) {
+            case Vak.HERHAAL:
+                posBuf += posBuf - player[Player.Beurt].pos;
+                playerTurn(id,posBuf);
+                break;
+            case Vak.BRUG:
+                posBuf = 12;
+                break;
+            case Vak.HERBERG:
+                player[Player.Beurt].beurtOverslaan = true;
+                break;
+            case Vak.PUT,Vak.GEVANGENIS:
+                for(int i = 0;i < Player.Count;i++){
+                    if(i != Player.Beurt && player[i].pos < player[Player.Beurt].pos){
+                        player[Player.Beurt].playersBehind++;
+                    }
+                }
+                if (player[Player.Beurt].playersBehind == 0) {
+                    player[Player.Beurt].beurtOverslaan = true;
+                }
+                break;
+            case Vak.VERDWAALD:
+                posBuf = 37;
+                break;
+            case Vak.DOOD:
+                posBuf = 0;
+                break;
+        }
+        player[Player.Beurt].movePlayer(posBuf);
+    }
+    public static void main(String[] args){
+        for(int i = 9;i < BOARDSZ;i+=9){
+            board[i]   = Vak.HERHAAL;
+            board[i-4] = Vak.HERHAAL;
+        }
+
+        board[6]  = Vak.BRUG;
+        board[19] = Vak.HERBERG;
+        board[31] = Vak.PUT;
+        board[42] = Vak.VERDWAALD;
+        board[52] = Vak.GEVANGENIS;
+        board[58] = Vak.DOOD;
+
+        for(int i = BOARDSZ;i < BOARDSZ+12;i++){
+            board[i] = Vak.TE_VER;
+        }
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welkom bij ganzeboard!");
+        System.out.println("hoeveel spelers zijn er?");
+        Player.Count = scan.nextInt();
+        player = new Player[Player.Count];
+
+        for (int i = 0; i < Player.Count; i++) {
+            player[i] = new Player();
+            System.out.print("Welke kleur?");
+            player[i].kleur = scan.next();
+        }
+        int highestThrow = 0;
+
+        for (int i = 0; i < Player.Count; i++) {
+            int dobbelWaarde = throwDice();
+            System.out.println("speler " + player[i].kleur + " heeft " + dobbelWaarde + " gegooit");
+            if(dobbelWaarde > highestThrow){
+                Player.Beurt = i;
+                highestThrow = dobbelWaarde;
+            }
+        }
+        System.out.println("speler " + player[Player.Beurt].kleur + " mag beginnen");
+        for(int i = 0;i < Player.Count;i++){
+            System.out.println("speler " + player[Player.Beurt].kleur + " is aan de beurt");
+            scan.next();
+            int dice1 = throwDice();
+            int dice2 = throwDice();
+
+            System.out.println("je hebt " + (dice1 + dice2) + " gegooit");
+
+            if((dice1==4&&dice2==5)||(dice1==5&&dice2==4)){
+                player[Player.Beurt].pos = 53;
+            }
+            else if((dice1==3&&dice2==6)||(dice1==6&&dice2==3)){
+                player[Player.Beurt].pos = 26;
+            }
+            else{
+                playerTurn(i,dice1+dice2);
+            }
+            Player.Beurt++;
+            if(Player.Beurt == Player.Count){
+                Player.Beurt = 0;
+            }
+            printBoard();
+        }
+        for(;;){
+            for(int i = 0;i < Player.Count;i++) {
+                System.out.println("speler " + player[Player.Beurt].kleur + " is aan de beurt");
+                if(player[Player.Beurt].beurtOverslaan){
+                    player[Player.Beurt].beurtOverslaan = false;
+                }
+                else if(player[Player.Beurt].playersBehind > 0){
+                    int playersBehind = 0;
+                    for(int i2 = 0;i2 < Player.Count;i2++){
+                        if(player[Player.Beurt].pos > player[i2].pos){
+                            playersBehind++;
+                        }
+                    }
+                    if(playersBehind != player[Player.Beurt].playersBehind){
+                        player[Player.Beurt].playersBehind = 0;
+                        scan.next();
+                        int diceValue = throwDice() + throwDice();
+                        System.out.println("je hebt " + diceValue + " gegooit");
+                        playerTurn(Player.Beurt,diceValue);
+                    }
+                }
+                else{
+                    scan.next();
+                    int diceValue = throwDice() + throwDice();
+                    System.out.println("je hebt " + diceValue + " gegooit");
+                    playerTurn(Player.Beurt,diceValue);
+                }
+                Player.Beurt++;
+                if(Player.Beurt == Player.Count){
+                    Player.Beurt = 0;
+                }
+                printBoard();
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
